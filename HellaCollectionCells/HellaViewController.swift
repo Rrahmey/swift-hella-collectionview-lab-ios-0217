a3237b504f5d82c8fe7ab9808f488a7d73a0dcf1
Raquel Rahmey <rrahmey@aol.com>
2017-02-16 10:10:26 -0500

    completed

--- HellaCollectionCells/HellaViewController.swift
+++ HellaCollectionCells/HellaViewController.swift
@@ -8,15 +8,39 @@
 
 import UIKit
 
-class HellaViewController: UIViewController {
+class HellaViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var HellaView: UICollectionView!
+    
     override func viewDidLoad() {
-    // Do any additional setup after loading the view, typically from a nib.
+    super.viewDidLoad()
+        HellaView.delegate = self
+        HellaView.dataSource = self
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath)
+        print(indexPath.item)
+        switch indexPath.item {
+        case 1,2,3,5,8,13,21,34,55,89:
+            cell.backgroundColor = UIColor.purple
+        default:
+            cell.backgroundColor = UIColor.yellow
+        }
+        
+        return cell
+    }
+    
+    
+    
 }
 
